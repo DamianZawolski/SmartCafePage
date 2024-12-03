95e5ba1dd94031053bf3ee0d160ac3722f9c5aca
Damian Zawolski <116562556+DamianZawolski@users.noreply.github.com>
2024-12-03 02:10:49 +0100

    Update firebase.js

--- smartcafe-page/src/components/firebase.js
+++ smartcafe-page/src/components/firebase.js
@@ -7,13 +7,6 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBhjdbh0zX_NtW1QZk0rKINPNyo4Po3y_Q",
-  authDomain: "smartcafe-2ad2d.firebaseapp.com",
-  projectId: "smartcafe-2ad2d",
-  storageBucket: "smartcafe-2ad2d.firebasestorage.app",
-  messagingSenderId: "1018869454751",
-  appId: "1:1018869454751:web:5c3161fa80adfb2bb08faa",
-  measurementId: "G-GJ7WMY7PG9"
 };
 
 // Initialize Firebase
